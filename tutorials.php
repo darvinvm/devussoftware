27ba5aea9e73771edadabba836fc40edcc83d5f4
darvinvm <darvinvm@users.noreply.github.com>
2020-01-14 22:17:02 -0400

    Footer Corregido

--- tutorials.php
+++ tutorials.php
@@ -3,40 +3,39 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no">
-<title>Hostio</title>
+<title>Devus Software
+</title>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-
 <nav id="mainNav" class="navbar navbar-default navbar-full">
-    <div class="container container-nav">
-        <div class="navbar-header">
-            <button aria-expanded="false" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs">
+		<div class="container container-nav">
+			<div class="navbar-header">
+				<button aria-expanded="false" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand page-scroll" href="index.html">
-                <img class="logo" src="images/logo.png" alt="Hostio">
+            <a class="navbar-brand page-scroll" href="index.php">
+                <img class="logo" src="images/logo.png" alt="Devus">
             </a>
         </div>
         <div style="height: 1px;" role="main" aria-expanded="false" class="navbar-collapse collapse" id="bs">
              <ul class="nav navbar-nav navbar-right">
-                <li><a href="about.html">About us</a></li>
-                <li class="dropdown">
-                    <a href="hosting.html">Hosting <span class="caret"></span></a>
+                <li><a href="about.php">Sobre Nosotros</a></li>
+                <li class="dropdown ">
+                    <a href="servicios.php">Servicios<span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                      <li><a href="whosting.html">Web Hosting</a></li>
-                      <li><a href="chosting.html">Cloud Hosting</a></li>
-                      <li><a href="dhosting.html">Dedicated Hosting</a></li>
+                      <li><a href="desarrolloweb.php">Desarrollo Web</a></li>
+                      <li><a href="sistemasweb.php">Sistemas Web</a></li>
+                      <li><a href="lineagrafica.php">Línea Gráfica/Branding</a></li>
                     </ul>
                 </li>
-                <li><a href="support.html">Help & Support</a></li>
-                <li><a href="contact.html">Contact us</a></li>
-                <li><a class="login-button" href="login.html">Login</a></li>
+                <li><a href="support.php">Ayuda & Soporte</a></li>
+                <li><a href="contact.html">Contactanos</a></li>
                 <li><a class="chat-button" href="#">Chat now</a></li>
             </ul>
         </div>
@@ -46,7 +45,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <div class="page-title">Customer Portal</div>
+                <div class="page-title">Tutorials</div>
                 <div class="page-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
             </div>
         </div>
